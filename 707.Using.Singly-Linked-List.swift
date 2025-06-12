bda677a4b5dba2e5577b11274aabf6d1f4061997
Emjumaev <mekhriddin312@gmail.com>
2025-06-13 00:58:52 +0500

    problem 707

--- 707.Using.Singly-Linked-List.swift
+++ 707.Using.Singly-Linked-List.swift
@@ -8,30 +8,115 @@
  *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
  * }
  */
-class Solution {
-    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        let dummy = ListNode(0)
-        var head: ListNode? = dummy
-
-        var head1: ListNode? = list1
-        var head2: ListNode? = list2
-
-        while(head1 != nil && head2 != nil) {
-            if head1!.val < head2!.val {
-                head!.next = head1
-                head1 = head1!.next
-            } else {
-                head!.next = head2
-                head2 = head2!.next
-            }
-            head = head!.next
-        }
-        
-        head!.next = head1 ?? head2
-
-        return dummy.next
+class Node {
+    var val: Int
+    var next: Node?
+    
+    init(val: Int, next: Node? = nil) {
+        self.val = val
+        self.next = next
     }
 }
 
-// Time: O(n + m)
-// Space: O(1)
+class MyLinkedList {
+    
+    var head: Node?
+
+    init() {
+        
+    }
+    
+    // O(n)
+    func get(_ index: Int) -> Int {
+        // O(n)
+        if head == nil {
+            return -1
+        } else {
+            var cur = head
+            var i = 0
+            
+            while(cur != nil) {
+                if index == i {
+                    return cur!.val
+                }
+                cur = cur!.next
+                i += 1
+            }
+        }
+        return -1
+    }
+    
+    // O(1)
+    func addAtHead(_ val: Int) {
+        let newNode = Node(val: val)
+        newNode.next = head
+        head = newNode
+    }
+    
+    // O(1)
+    func addAtTail(_ val: Int) {
+        var cur = head
+        let newNode = Node(val: val)
+
+        if head == nil {
+            head = newNode
+            return
+        }
+        
+        while(cur?.next != nil) {
+            cur = cur?.next
+        }
+        
+        cur?.next = newNode
+    }
+    
+    // O(n)
+    func addAtIndex(_ index: Int, _ val: Int) {
+        var prev: Node? = nil
+        var cur = head
+        var i = 0
+        
+        if head == nil && index == 0 {
+            addAtHead(val)
+            return
+        }
+        
+        while(cur != nil) {
+            if index == i {
+                if prev == nil {
+                    addAtHead(val)
+                } else {
+                    let newNode = Node(val: val)
+                    prev!.next = newNode
+                    newNode.next = cur
+                }
+            }
+            i += 1
+            prev = cur
+            cur = cur!.next
+        }
+        if index == i {
+            addAtTail(val)
+        }
+    }
+    
+    // O(n)
+    func deleteAtIndex(_ index: Int) {
+        var prev: Node?
+        var cur = head
+        var i = 0
+        
+        while(cur != nil) {
+            if i == index {
+                if prev == nil {
+                    head = cur?.next
+                } else {
+                    prev?.next = cur?.next
+                }
+            }
+            i += 1
+            prev = cur
+            cur = cur?.next
+        }
+    }
+}
