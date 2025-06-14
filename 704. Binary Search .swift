7df692f1d78d0763e6a417425a88aa94b7ef96e3
Emjumaev <mekhriddin312@gmail.com>
2025-06-15 00:21:50 +0500

    problem 74

--- 704. Binary Search .swift
+++ 704. Binary Search .swift
@@ -1,59 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init() { self.val = 0; self.next = nil; }
- *     public init(_ val: Int) { self.val = val; self.next = nil; }
- *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
- * }
- */
-
 class Solution {
-    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        // Binary Search
+        var left = 0
+        var right = nums.count - 1
 
-        if lists.count == 0 {
-            return nil
-        }
+        while(left <= right) {
+            var middle = (left + right) / 2
 
-        var i = 1
-        var initial: ListNode? = lists[0]
-        
-        // Time: O(k)
-        while(i < lists.count) {
-            initial = mergeTwoLinkedLists(list1: initial, list2: lists[i])
-            i += 1
-        }
-        
-        return initial
-    }
-    
-    // Merge two linked lists into one
-    func mergeTwoLinkedLists(list1: ListNode?, list2: ListNode?) -> ListNode? {
-        var node1 = list1
-        var node2 = list2
-        let result: ListNode? = ListNode()
-        var cur: ListNode? = result
-        
-        while(node1 != nil && node2 != nil) {
-            if node1!.val < node2!.val {
-                cur!.next = node1
-                node1 = node1!.next
+            if nums[middle] > target {
+                right = middle - 1
+            } else if nums[middle] < target {
+                left = middle + 1
             } else {
-                cur!.next = node2
-                node2 = node2!.next
+                return middle
             }
-            cur = cur!.next
         }
-        
-        if node1 == nil {
-            cur!.next = node2
-        } else {
-            cur!.next = node1
-        }
-        return result!.next
+        return -1
     }
 }
-
-// Time: O(n * k)
-// Space: O(1)
