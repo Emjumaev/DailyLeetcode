10902918ef6acce001c0069e524da60c706f78a6
Emjumaev <mekhriddin312@gmail.com>
2025-07-08 20:19:51 +0500

    Problem 191

---
+++ 191. Number of 1 Bits.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func hammingWeight(_ n: Int) -> Int {
+        var count = 0
+        var num = n
+
+        while(num > 0) {
+            if num & 1 == 1 {
+                count += 1
+            }
+            num = num >> 1
+        }
+        
+        return count
+    }
+}
