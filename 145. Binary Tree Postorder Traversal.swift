00289e61eddec3ee99de35fb987a39dd8b4b4a17
Mehriddin Jumaev <214824116+Emjumaev@users.noreply.github.com>
2025-10-08 18:10:56 +0500

    Problem 145

---
+++ 145. Binary Tree Postorder Traversal.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var res = [Int]()
+
+        func postOrder(_ root: TreeNode?) {
+            if root == nil {
+                return
+            }
+            postOrder(root?.left)
+            postOrder(root?.right)
+            res.append(root!.val)
+        }
+
+        postOrder(root)
+        return res
+    }
+}
