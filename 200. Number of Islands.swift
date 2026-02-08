f9535f05d9b4624ee7c4da740282ead52251084b
Emjumaev <mekhriddin312@gmail.com>
2026-02-08 22:59:36 +0500

    Problem 695

--- 200. Number of Islands.swift
+++ 200. Number of Islands.swift
@@ -1,32 +1,33 @@
-import Foundation
+class Solution {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        var grid = grid
 
-func selectionSort(arr: [Int]) -> [Int] {
-    var arr = arr
-    
-    if arr.isEmpty {
-        return []
-    }
-    
-    for i in 0...arr.count - 1 {
-        var minIndex = i
-        
-        // Find the min element
-        for j in minIndex...arr.count - 1 {
-            if arr[minIndex] > arr[j] {
-                minIndex = j
+        var res = 0
+        var rowCount = grid.count
+        var colCount = grid[0].count
+
+        func dfs(_ row: Int, _ col: Int) {
+            if row < 0 || col < 0 || row == rowCount || col == colCount || grid[row][col] == "0" {
+                return
+            }
+
+            grid[row][col] = "0"
+
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+        }
+
+        for row in 0..<rowCount {
+            for col in 0..<colCount {
+                if grid[row][col] == "1" {
+                    dfs(row, col)
+                    res += 1
+                }
             }
         }
-        
-        // Do Swap operation
-        if i != minIndex {
-            (arr[i], arr[minIndex]) = (arr[minIndex], arr[i])
-        }
+
+        return res
     }
-    
-    return arr
 }
-
-let test = selectionSort(arr: [4, 2, 7, 3, 0, 5])
-print(test)
-
-// Just a commit
